319280408fc5046fa9f98c9e821ea2c02f6415c8
abdoo <abdalrhmanashraf@gmail.com>
2024-08-10 12:20:32 +0300

    + night mode

--- app/src/main/java/com/example/task_1facebook/MainActivity.java
+++ app/src/main/java/com/example/task_1facebook/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
 
         etMobileOrEmail = findViewById(R.id.et_mobile_or_email);
         etPassword = findViewById(R.id.et_password);
@@ -27,17 +30,14 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(v -> {
             String mobileOrEmail = etMobileOrEmail.getText().toString().trim();
             String password = etPassword.getText().toString().trim();
-            // Handle login logic here
             Toast.makeText(MainActivity.this, "Login clicked", Toast.LENGTH_SHORT).show();
         });
 
         btnCreateAccount.setOnClickListener(v -> {
-            // Handle create account logic here
             Toast.makeText(MainActivity.this, "Create new account clicked", Toast.LENGTH_SHORT).show();
         });
 
         tvForgotPassword.setOnClickListener(v -> {
-            // Handle forgot password logic here
             Toast.makeText(MainActivity.this, "Forgot password clicked", Toast.LENGTH_SHORT).show();
         });
     }
